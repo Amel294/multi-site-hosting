50eb619902030d749b75cb7033b0d5ad244e3e9f
Amel Umesh <45140781+Amel294@users.noreply.github.com>
2024-11-04 02:01:59 +0530

    made changes to port

--- site2/vite.config.js
+++ site2/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: '0.0.0.0',  // Listen on all network interfaces
-    port: 4173,
+    port: 4174,
   },
   plugins: [react()],
 })
